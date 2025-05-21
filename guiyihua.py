26f453aee8a3747aaeb9b7eb092b6162d0a7ebd0
XJasonLinX <129581260+XJasonLinX@users.noreply.github.com>
2025-05-21 09:10:40 +0800

    Update guiyihua.py

--- guiyihua.py
+++ guiyihua.py
@@ -3,9 +3,9 @@ import pandas as pd
 import warnings
 
 # Data loading and preprocessing
-feature = pd.read_csv('pre18282.csv')
-labels = np.array(feature['D'])
-feature = feature.drop(columns=['D'])
+feature = pd.read_csv('truedata.csv')
+labels = np.array(feature['results'])
+feature = feature.drop(columns=['results'])
 print("数据维度", feature.shape)
 feature = np.array(feature)
 
